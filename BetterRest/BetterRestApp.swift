85e9efbccbc41dd708cec1f8dc716834a31d3cc2
nusaibatron <nusaiba@berkeley.edu>
2022-08-23 16:13:21 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nusaiba Rahman on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
