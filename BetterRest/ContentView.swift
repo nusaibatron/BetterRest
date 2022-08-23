6c5bff265451f1d101c9fb180be458e54f7c6ea4
nusaibatron <nusaiba@berkeley.edu>
2022-08-23 18:38:10 -0400

    Overview commit

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var sleepAmount = 0
+    @State var wakeUpTime = Date.now
+    
     var body: some View {
+        
+        
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 1)
+        
+        DatePicker("Select when to wake up", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
+        
+        
+
         Text("Hello, world!")
             .padding()
+    }
+    
+    func trivialElement() {
+        var components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+
     }
 }
 
